9313ef1ed40ae05aad467ab5ed388a1ca6481669
Luca Vieira <lucamvieira@protonmail.com>
2024-02-19 16:42:02 -0300

    projeto finalizada 8

--- django2/settings.py
+++ django2/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dseo=-4d14+j=9ei*7u3$0*!g8pmq*)$5qsh&#z^^&45t^37ho'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
