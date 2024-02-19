33089e9e93327ff992f42322899929b7b28c3663
Luca Vieira <lucamvieira@protonmail.com>
2024-02-19 15:36:01 -0300

    projeto finalizada 6

--- django2/settings.py
+++ django2/settings.py
@@ -16,8 +16,7 @@ import dj_database_url
 import environ
 
 #Initialise our environment variables
-
-env = environ.Env()
+env= environ.Env()
 
 environ.Env.read_env()
 
@@ -33,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dseo=-4d14+j=9ei*7u3$0*!g8pmq*)$5qsh&#z^^&45t^37ho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
